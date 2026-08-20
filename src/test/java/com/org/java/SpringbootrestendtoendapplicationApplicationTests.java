001240c9ac9cd7ec6d626f5c90aad060cf9358f4
javatechivkp <sreenivasarao.javasoft9@gmail.com>
2026-08-20 14:39:09 +0530

    newcongadded2

--- src/test/java/com/org/java/SpringbootrestendtoendapplicationApplicationTests.java
+++ src/test/java/com/org/java/SpringbootrestendtoendapplicationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootrestendtoendapplicationApplicationTests {
 
-    @Test
+    /*@Test
     void contextLoads() {
-    }
+    }*/
 
 }
